be9680a2feaaa07a04ac7ab456d161062b8f7803
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-01-05 21:27:24 +0900

    clean up

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,6 @@
 import "@/app/globals.css"
 import type { Preview } from "@storybook/react"
 
-const preview: Preview = {
-	parameters: {
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-		},
-	},
-}
+const preview: Preview = {}
 
 export default preview
