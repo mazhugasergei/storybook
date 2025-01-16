5241d6ff08f3c4a726416036dddc2e5087124cd4
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-01-16 20:34:56 +0900

    add input

--- components/Button.stories.ts
+++ components/Button.stories.ts
@@ -1,4 +1,4 @@
-import { Button, defaultVariants, variants } from "@/components/Button"
+import { Button, defaultVariants, variants } from "@/components/button"
 import type { Meta, StoryObj } from "@storybook/react"
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
---
+++ components/Input.stories.ts
@@ -0,0 +1,57 @@
+import { Input } from "@/components/input"
+import type { Meta, StoryObj } from "@storybook/react"
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof Input> = {
+	title: "Input",
+	component: Input,
+	parameters: {
+		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+		layout: "centered",
+	},
+	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+	tags: ["autodocs"],
+	// More on argTypes: https://storybook.js.org/docs/api/argtypes
+	argTypes: {
+		ref: {
+			table: { disable: true },
+			control: false,
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+const defaultArgs: Partial<Story["args"]> = {
+	placeholder: "Placeholder",
+}
+
+export const Text: Story = {
+	args: {
+		...defaultArgs,
+		type: "text",
+	},
+}
+
+export const Number: Story = {
+	args: {
+		...defaultArgs,
+		type: "number",
+	},
+}
+
+export const Password: Story = {
+	args: {
+		...defaultArgs,
+		type: "password",
+	},
+}
+
+export const Date: Story = {
+	args: {
+		...defaultArgs,
+		type: "date",
+	},
+}
